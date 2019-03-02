6cb6c605f607deabad81d902b36d7cde2f9dbd3f
Vlad Bezden <vlad.bezden@gmail.com>
2019-03-02 06:08:26 -0500

    added parameter for progress bar size

--- autoclicker/autoclicker.py
+++ autoclicker/autoclicker.py
@@ -4,6 +4,7 @@ import ctypes
 import time
 from sys import stdout
 from enum import IntFlag
+from functools import partial
 
 
 class Status(IntFlag):
@@ -14,9 +15,9 @@ class Status(IntFlag):
 status = Status.RUNNING
 
 
-def progress_bar(value: int, total: int, prefix: str = "", size: int = 60):
+def progress_bar(value: int, total: int, size: int = 60):
     x = int(size * value / total)
-    stdout.write(f"{prefix}[{'#' * x}{'.' * (size - x)}] {value}/{total}\r")
+    stdout.write(f"[{'#' * x}{'.' * (size - x)}] {value}/{total}\r")
     stdout.flush()
 
 
@@ -26,13 +27,13 @@ def mouse_click():
     ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)
 
 
-async def click(event, delay):
+async def click(event, delay, pb):
     while await asyncio.wait([event.wait()]):
         await asyncio.sleep(0.1)
         print("\r")
         for i in range(delay + 1):
             if status == Status.RUNNING:
-                progress_bar(i, delay)
+                pb(i)
                 await asyncio.sleep(1)
             else:
                 break
@@ -50,11 +51,12 @@ def prompter(loop, event):
 
 
 def main():
-    interval = parse_args()
+    args = parse_args()
+    pb = partial(progress_bar, size=args.size, total=args.interval)
     event = asyncio.Event()
     event.set()
     loop = asyncio.get_event_loop()
-    loop.create_task(click(event, interval))
+    loop.create_task(click(event, args.interval, pb))
     loop.run_in_executor(None, prompter, loop, event)
     try:
         loop.run_forever()
@@ -67,6 +69,7 @@ def main():
 def parse_args():
     parser = argparse.ArgumentParser(description="Simulate user mouse click")
     interval = 60
+    size = 60
     parser.add_argument(
         "-i",
         "--interval",
@@ -74,12 +77,19 @@ def parse_args():
         default=interval,
         help=f"Time in seconds mouse will click. Default time is {interval} secs",
     )
-    interval = parser.parse_args().interval
-    print(
-        f"Mouse will click every {interval} seconds\n"
-        "Press Enter for pause and resume program\n"
+    parser.add_argument(
+        "-s",
+        "--size",
+        type=int,
+        default=size,
+        help=f"Size of the progress bar. Default size is {size} chars",
     )
-    return interval
+    args = parser.parse_args()
+    print(
+        f"Mouse will click every {args.interval} seconds. Progress bar size {args.size}"
+        "\nPress Enter for pause and resume program\n"
+    )
+    return args
 
 
 if __name__ == "__main__":
